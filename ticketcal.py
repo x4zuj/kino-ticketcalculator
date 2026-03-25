dce83a502688d4ae4bff3aca411d81e3ffadba13
x4zuj <nikita.kern09@web.de>
2026-03-25 15:46:12 +0100

    Enhance ticket calculator with price change notifications
    Added print statements to indicate price changes based on age and day of the week.

--- ticketcal.py
+++ ticketcal.py
@@ -1,5 +1,6 @@
-
 # made by 4zuj
+# I made a ticket calculator!
+# If you look in my other projects, you can see that I always start with a greeting :D
 
 print("Greetings, welcome to the website of the cinema XY!")
 
@@ -9,6 +10,7 @@ q = int(input("How old are you?: "))
 if q < 12:
     print("Sorry, you are not allowed to see this movie.")
     exit()
+    # I made exit(), since if the User is below of the age of 12, he isn't allowed to go to the cinema.
 w = input("Would you like to watch 2D or 3D?: ")
 e = input("Would you like to watch the movie in the week or on the weekend?: ")
 r = input("Do you have a customer card?: ")
@@ -22,16 +24,16 @@ adv = 14
 if q == 13 or q == 14:
     norm = norm-3
     adv = adv-3
-    print("Price changed")
+    print("!!! Price changed !!!") # I added a printout, so I could see, if the price has changed.
 elif q >= 65:
     norm = norm-2
     adv = adv-2
-    print("Price changed")
+    print("!!! Price changed !!!") # also just a printout
 
 if e == "weekend":
     norm = norm+2
     adv = adv+2
-    print("Price changed")
+    print("!!! Price changed !!!") # you know it.
 else:
     norm = norm
     adv = adv
